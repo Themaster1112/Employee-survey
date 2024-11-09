9a00f41ea38d304edeaa844f5bc5c8de6f1bb012
Pythonforbasics <114282587+Themaster1112@users.noreply.github.com>
2024-11-09 18:45:24 +0000

    first commit

---
+++ MAINCODE/SURVEY.py
@@ -0,0 +1,6 @@
+SURVEY=input("THE Company needs you to answer this question. Are you happy with the sallery? say Yes or No this code is sensetive to the input toy give it: ")
+if SURVEY == "Yes":
+    print("Thank you.")
+else:
+    WHYNOT=input("Why are you not happy about your sallery?")
+    print("Thank you for your input")
